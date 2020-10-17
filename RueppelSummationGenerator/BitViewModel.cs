eeac9e42c574f842e69bdd1819437e4f3382732e
KonradKarkos <saxim826@gmail.com>
2020-10-17 21:59:05 +0200

    Not important changes in BitViewModel (deleted useless code)

--- RueppelSummationGenerator/BitViewModel.cs
+++ RueppelSummationGenerator/BitViewModel.cs
@@ -6,46 +6,12 @@ namespace SummationGenerator
 {
     class BitViewModel
     {
-        private ObservableCollection<Bit> BitList;
 
         public BitViewModel()
         {
-            BitList = new ObservableCollection<Bit>();
+            Bits = new ObservableCollection<Bit>();
         }
 
-        public ObservableCollection<Bit> Bits
-        {
-            get { return BitList; }
-            set { BitList = value; }
-        }
-
-        private ICommand mUpdater;
-        public ICommand UpdateCommand
-        {
-            get
-            {
-                if (mUpdater == null)
-                    mUpdater = new Updater();
-                return mUpdater;
-            }
-            set
-            {
-                mUpdater = value;
-            }
-        }
-
-        private class Updater : ICommand
-        {
-            public bool CanExecute(object parameter)
-            {
-                return true;
-            }
-            public event EventHandler CanExecuteChanged;
-
-            public void Execute(object parameter)
-            {
-
-            }
-        }
+        public ObservableCollection<Bit> Bits { get; set; }
     }
 }
